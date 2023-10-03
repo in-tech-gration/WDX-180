8a5f0f3ca2106ac96e0a0f5d78554f6ff7887ff4
Kostas Minaidis <minaidisk@gmail.com>
2023-10-04 01:27:08 +0300

    U

---
+++ user/week01/exercises/day05/javascript-first-steps/largest-number.test.js
@@ -0,0 +1,10 @@
+const test = require('node:test');
+const assert = require('assert')
+
+test('should add two numbers', () => {
+        assert.equal(10, 10)
+})
+
+test('should subtract two numbers', () => {
+        assert.equal(10, 20)
+})
