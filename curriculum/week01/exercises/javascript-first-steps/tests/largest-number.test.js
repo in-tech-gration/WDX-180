923caf9c91eef93c0047e826b199495ef5514556
Kostas Minaidis <minaidisk@gmail.com>
2023-10-05 02:51:10 +0300

    [ CURRICULUM ][ WEEK 01 ] Added GitHub actions for 1st JS exercise test

---
+++ curriculum/week01/exercises/javascript-first-steps/tests/largest-number.test.js
@@ -0,0 +1,25 @@
+const test = require('node:test');
+const assert = require('assert')
+
+try {
+
+  require("../../../../../user/week01/exercises/day05/javascript-first-steps/largest-number");
+
+} catch(e){
+
+  throw new Error("Could not find exercise solution at the user/ folder. Please check if you have used the correct folder path and commit message");
+
+}
+
+test('should find the max number', () => {
+
+  const checkMax1 = findMaxNumber(10, 5);
+  const checkMax2 = findMaxNumber(10, 15);
+  const checkMax3 = findMaxNumber(100, 100);
+
+  assert.equal(checkMax1, 10)
+  assert.equal(checkMax2, 15)
+  assert.equal(checkMax3, 100)
+
+});
+
