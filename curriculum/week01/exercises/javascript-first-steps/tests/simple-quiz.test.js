4af9774cf68778ee07bdcee7dc4355438ebad295
Asteris Giannoudis <asteris.gian@gmail.com>
2023-10-09 16:03:42 +0300

    [ CURRICULUM ][ WEEK 01 ] Updated JS tests.

--- curriculum/week01/exercises/javascript-first-steps/tests/simple-quiz.test.js
+++ curriculum/week01/exercises/javascript-first-steps/tests/simple-quiz.test.js
@@ -2,7 +2,7 @@ const test = require('node:test');
 const assert = require('assert')
 
 const solutionPath = "user/week01/exercises/day05/javascript-first-steps/simple-quiz"
-global.prompt = () => {};
+global.prompt = () => { return '' };
 global.alert = () => {};
 
 try {
