27f4fb8d96fe2ecd03b16bc14659ae28d552a8ad
Kostas Minaidis <minaidisk@gmail.com>
2023-10-23 22:35:25 +0300

    [ JEKYLL ] Excluding Modules folder. Updated sgen.js [ skip ci ]

--- tools/sgen.js
+++ tools/sgen.js
@@ -1,5 +1,9 @@
 // DESCRIPTION: Script to dynamically generate Syllabus and related content in Markdown format
 // USAGE: WDX-180/> node tools/sgen.js curriculum/curriculum.yaml
+/**
+ * REPLACES: {{ WDX:Syllabus }} => curriculum.yaml
+ * REPLACES: {{ WDX:WEEK }}     => weekly.yaml
+ */
 
 // 0) IMPORTS: =================================================================
 const path = require("node:path");
@@ -33,11 +37,11 @@ function getFrontMatterStringFromObject(fm) {
 
 }
 
-function createSyllabusEntries(syllabus, messages){
+function createSyllabusEntries(syllabus, messages) {
 
   let syllabusText = "";
   const syllabusEntries = Object.entries(syllabus);
-  
+
   syllabusEntries.forEach(([key, value], i, s) => {
 
     syllabusText += `### ${value.title}\n\n`
@@ -125,10 +129,72 @@ function createSyllabusFromMarkdownText({ configYaml, textContent }) {
 
 }
 
-function createWeeklyContentFromYaml({ configYaml }){
+function createWeeklyContentFromYaml({ configYaml, filename }) {
 
-  const configYamlObject = yaml.parse(configYaml);
-  console.log({ configYamlObject });
+  const { input, schedule, title } = yaml.parse(configYaml);
+  const weeklyFolder = path.join("curriculum", filename);
+  const weeklyFolderExists = fs.existsSync(weeklyFolder)
+
+  if ( weeklyFolderExists ) {
+
+    console.log(`Folder '${filename}' already exists.`);
+    
+  } else {
+    
+    fs.mkdirSync(weeklyFolder);
+    console.log(`Folder '${filename}' created.`);
+    
+  }
+
+  const textContent = fs.readFileSync(input, "utf-8");
+
+  // Parse markdown and separate Frontmatter and main content:
+  const { content, data: fm, orig } = matter(textContent);
+  // console.log({fm}); // { title: '{{ WDX:WEEK }} | {{ WDX:TITLE }}' }
+
+  const numOfWeek = filename.split("week")[1]; // 04
+
+  // Mini-parsers: (to be moved elsewhere and unit-tested)
+  function parseWeeklyPatterns({ raw, numOfWeek }){
+
+    const weekRegex        = /\{\{\s?WDX:\s?WEEK\s?\}\}/gi;
+    const titleRegex       = /\{\{\s?WDX:\s?TITLE\s?\}\}/gi;
+    const dateUpdatedRegex = /\{\{\s?WDX:\s?DATE_UPDATED\s?\}\}/gi;
+
+    const date = new Date();
+    const DDMMYYYY = `${date.getDate()}/${date.getMonth()+1}/${date.getFullYear()}` 
+
+    let newRaw = raw
+    .replace(weekRegex, `Week ${numOfWeek}`)
+    .replace(titleRegex, title)
+    .replace(dateUpdatedRegex, DDMMYYYY);
+
+    return newRaw;
+  }
+
+  // Parse markdown tokens:
+  const markdownTokens = marked.lexer(content);
+  let outputContent = "";
+  markdownTokens.forEach( token =>{
+    if ( token.raw ){
+      const parsedTokenRaw = parseWeeklyPatterns({ raw: token.raw, numOfWeek }); 
+      outputContent += parsedTokenRaw;
+    } else {
+      outputContent += token.raw;
+    }
+  });
+
+  const fmString = getFrontMatterStringFromObject(fm);
+
+  outputContent = parseWeeklyPatterns({ raw: fmString, numOfWeek }) + outputContent;
+
+  const weeklyIndexMarkdown = path.join( weeklyFolder, "index.md" );
+  try {
+    fs.writeFileSync(weeklyIndexMarkdown, outputContent, "utf-8");
+  } catch(e){
+    console.log(e);
+  }
+
 
 }
 
@@ -148,18 +214,21 @@ function init() {
 
     const textContent = fs.readFileSync(input, "utf-8");
 
-    if ( Syllabus ){  // e.g. curriculum/curriculum.yaml
+    if (Syllabus) {  // e.g. curriculum/curriculum.yaml
 
       console.log(`Processing Syllabus: ${input}`);
       const outputContent = createSyllabusFromMarkdownText({ textContent, configYaml });
       fs.writeFileSync(output, outputContent, "utf-8");
-      
-    } else {  // e.g. curriculum/schedule/week04.yaml
-      
-      console.log(`Processing Weekly Content: ${input}`);
-      createWeeklyContentFromYaml({ configYaml: configYaml });
 
-      // fs.writeFileSync(output, outputContent, "utf-8");
+    } else {  // e.g. curriculum/schedule/week04.yaml
+
+      console.log(`Processing Weekly Content: ${configYamlPath}`);
+      const filename = path.basename(configYamlPath, path.extname(configYamlPath));
+      if ( filename.indexOf("week") !== 0 ){
+        return warn("Weekly YAML requires the following format: 'weeklyDD.yaml'")
+      } else {
+        createWeeklyContentFromYaml({ configYaml, filename });
+      }
 
     }
 
