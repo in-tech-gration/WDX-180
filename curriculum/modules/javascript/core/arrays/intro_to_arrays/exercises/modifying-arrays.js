c4d985b315aeb6aeadc0463a502053269cafb913
Asteris Giannoudis <asteris.gian@gmail.com>
2023-11-07 11:02:54 +0200

    [ MODULES ] Moved remaining W06 exercises to external files.

---
+++ curriculum/modules/javascript/core/arrays/intro_to_arrays/exercises/modifying-arrays.js
@@ -0,0 +1,16 @@
+/* 
+------------------------------------------------------------------------------------
+Tutorial: Modify an array using indexes
+------------------------------------------------------------------------------------
+*/
+
+// The most basic way to modify an array is to use indexes. We replace the element at the 0th index by 5.
+var numbers = [1, 2, 3, 4];
+numbers[0] = 5;
+console.log(numbers);
+
+/*
+------------------------------------------------------------------------------------
+Challenge: Given an array of positive integers above, if a number in the array of numbers is even, then multiply it by 2
+------------------------------------------------------------------------------------
+*/
---
+++ curriculum/modules/javascript/core/intro/exercises/loops.js
@@ -0,0 +1,14 @@
+/*
+
+  JavaScript Loops
+
+  Challenge:
+
+  -> declare two variables, "character" and "timesToRepeat"
+  -> using a loop, repeat each character that many times
+  -> console.log the output
+
+  example:  if I had character = 'f' and timesToRepeat = 5, it'd console.log 'fffff'
+
+  Try a few different combinations to make sure you got it right e.g. 'a' and 10, 'c' and 100, '🐶' and 3.
+*/
