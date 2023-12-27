b93404386c9c8dce2321ef7d38e8f72b293cd2cc
Kostas Minaidis <minaidisk@gmail.com>
2023-12-27 21:43:50 +0200

    [ NEWBIESCRIPT ] Added error notification for unhandled Promise rejection

--- curriculum/modules/newbiescript/newbiescript.js
+++ curriculum/modules/newbiescript/newbiescript.js
@@ -1,15 +1,22 @@
 ;(function(){
+  // cSpell: ignore newbiescript
+
+  // Minify: npx uglify-js newbiescript.js > newbiescript.min.js
 
   // XRAY:
   if ( !window.xray ){
     window.xray = (...args) => console.log("🩻", ...args);
   }
   
-  function renderErrorElement({ errorMessage, lineNumber }){
+  function renderErrorElement({ errorMessage, lineNumber, msg = null }){
 
     const errorEl = document.createElement("div");
     errorEl.textContent = `🛑 JS ERROR: ${errorMessage} (line: ${lineNumber})`;
     errorEl.style.cssText = `position:fixed; font-family:sans-serif; color:red; margin:8px; z-index:99999; cursor:pointer; top:0; right:0;`
+    if ( msg ){
+      errorEl.insertAdjacentHTML("beforeend",`<hr>`);
+      errorEl.insertAdjacentText("beforeend", msg);
+    }
     document.body.appendChild(errorEl);
 
   }
@@ -31,7 +38,22 @@
   });
   
   // https://developer.mozilla.org/en-US/docs/Web/API/Window/unhandledrejection_event
-  window.addEventListener("unhandledrejection", e => {});
+  window.addEventListener("unhandledrejection", e => {
+
+    try {
+
+      const stack      = e.reason.stack.split(":");
+      const col        = stack[stack.length-1]
+      const lineNumber = stack[stack.length-2];
+      renderErrorElement({ 
+        errorMessage: e.reason.message,
+        lineNumber,
+        msg: `There seems to be an unhandled Promise rejection. Did you forget the catch()?` 
+      });
+
+    } catch(e){}
+  
+  });
   
   // https://developer.mozilla.org/en-US/docs/Web/API/Window/rejectionhandled_event
   window.addEventListener("rejectionhandled", e => {});
