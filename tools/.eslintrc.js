48defe734e11a93597285a6f3ee2075f59f8e695
Kostas Minaidis <minaidisk@gmail.com>
2024-09-08 16:53:53 +0300

    Add node:true to ESLint config so that it stops complaining about __dirname and other Node vars

--- tools/.eslintrc.js
+++ tools/.eslintrc.js
@@ -2,7 +2,8 @@ module.exports = {
     "env": {
         "browser": true,
         "commonjs": true,
-        "es2021": true
+        "es2021": true,
+        "node": true
     },
     // "extends": "eslint:recommended",
     "overrides": [
