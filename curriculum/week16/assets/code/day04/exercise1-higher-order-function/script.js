74c9cddc0c1f09e6048c2b28a37708216db48778
Kostas Minaidis <minaidisk@gmail.com>
2025-01-28 01:12:46 +0200

    Update W16D04

---
+++ curriculum/week16/assets/code/day04/exercise1-higher-order-function/script.js
@@ -0,0 +1,54 @@
+console.clear();
+
+/*
+
+Setup:
+
+- Download the Codepen: Click the "Export" button (bottom right) and select "Export .ZIP"
+- Open it in your local environment (e.g. VSCode with Live Server)
+- Follow the instructions below:
+
+Instructions:
+
+In this page, there is an event handler called "click", that gets executed every time the user
+clicks the button. We need to count the times the user has toggled the button color using 
+a simple counter and display that counter in the console every time it happens.
+
+You should implement a "Higher Order Function" that turns the "click" function into a function
+that counts the number of clicks and displays them in the console. Here's the expected behaviour:
+
+User Clicks => color turns to green => "Toggled 1 times!" gets displayed in the console
+
+User Clicks => color turns to red => "Toggled 2 times!" gets displayed in the console
+
+User Clicks => color turns to green => "Toggled 3 times!" gets displayed in the console
+
+And so forth...
+
+To start, you must comment and uncomment the appropriate lines in the code and write your code 
+inside the tracker function WITHOUT changing the number of parameters accepted. You must not add 
+any code outside the tracker function in order to solve this exercise.
+
+When finished, copy your code in a folder named "higher-order-function" inside the 
+user/week16/exercises/day04/ folder, commit with the message "w16d04-higher-order-function" and push.
+
+Here is some reading:
+
+- https://www.freecodecamp.org/news/higher-order-functions-in-javascript-explained/
+- https://eloquentjavascript.net/05_higher_order.html#h_xxCc98lOBK
+
+*/
+
+
+function click(e){
+  e.target.style.background = e.target.style.background === "red" ? "green" : "red";  
+}
+function tracker( fn ){
+  // Your code here...
+}
+
+//>> Comment the following line:
+document.querySelector("button").addEventListener("click", click);
+
+//>> Uncomment the following line:
+// document.querySelector("button").addEventListener("click", tracker(click));
---
+++ curriculum/week16/assets/code/day04/exercise2-tictactoe/script.js
@@ -0,0 +1,114 @@
+console.clear();
+
+/*
+
+Setup:
+
+- Download the Codepen: Click the "Export" button (bottom right) and select "Export .ZIP"
+- Open it in your local environment (e.g. VSCode with Live Server)
+- Follow the instructions below:
+
+Instructions:
+
+A junior developer has created 9 event handlers for each of the Tic-Tac-Toe cells. We think
+you can do better than this and follow the DRY principle. Here's what you have to do:
+
+1) Try limiting the event handlers to a minimum.
+2) Turn the board green when X wins (there's a special CSS class for that called .win)
+3) Turn the board red when O wins (there's also a .lose class)
+4) Stop any moves from happening when either X or O wins or loses.
+5) Add a Reset or Restart button that starts the game again and clears the board
+
+When finished, copy your code in a folder named "tic-tac-toe" inside the 
+user/week16/exercises/day04/ folder, commit with the message "w16d04-tic-tac-toe" and push.
+
+*/
+
+let move = "player1";
+
+const cell1 = document.querySelector(".cell:nth-child(1)");
+const cell2 = document.querySelector(".cell:nth-child(2)");
+const cell3 = document.querySelector(".cell:nth-child(3)");
+const cell4 = document.querySelector(".cell:nth-child(4)");
+const cell5 = document.querySelector(".cell:nth-child(5)");
+const cell6 = document.querySelector(".cell:nth-child(6)");
+const cell7 = document.querySelector(".cell:nth-child(7)");
+const cell8 = document.querySelector(".cell:nth-child(8)");
+const cell9 = document.querySelector(".cell:nth-child(9)");
+
+clicks: {
+
+  // break clicks; // <= You can uncomment this line to temporarily disable the event listeners:
+  
+  cell1.addEventListener("click", event =>{
+    if ( cell1.innerText.trim().length !== 0 ){
+      return false;
+    }
+    cell1.innerText = move === "player1" ? "X" : "O";
+    move = move === "player1" ? "player2" : "player1";
+  });
+
+  cell2.addEventListener("click", event =>{
+    if ( cell2.innerText.trim().length !== 0 ){
+      return false;
+    }
+    cell2.innerText = move === "player1" ? "X" : "O";
+    move = move === "player1" ? "player2" : "player1";
+  });  
+
+  cell3.addEventListener("click", event =>{
+    if ( cell3.innerText.trim().length !== 0 ){
+      return false;
+    }
+    cell3.innerText = move === "player1" ? "X" : "O";
+    move = move === "player1" ? "player2" : "player1";
+  }); 
+  
+  cell4.addEventListener("click", event =>{
+    if ( cell4.innerText.trim().length !== 0 ){
+      return false;
+    }
+    cell4.innerText = move === "player1" ? "X" : "O";
+    move = move === "player1" ? "player2" : "player1";
+  });
+
+  cell5.addEventListener("click", event =>{
+    if ( cell5.innerText.trim().length !== 0 ){
+      return false;
+    }
+    cell5.innerText = move === "player1" ? "X" : "O";
+    move = move === "player1" ? "player2" : "player1";
+  });  
+
+  cell6.addEventListener("click", event =>{
+    if ( cell6.innerText.trim().length !== 0 ){
+      return false;
+    }
+    cell6.innerText = move === "player1" ? "X" : "O";
+    move = move === "player1" ? "player2" : "player1";
+  }); 
+
+  cell7.addEventListener("click", event =>{
+    if ( cell7.innerText.trim().length !== 0 ){
+      return false;
+    }
+    cell7.innerText = move === "player1" ? "X" : "O";
+    move = move === "player1" ? "player2" : "player1";
+  });
+
+  cell8.addEventListener("click", event =>{
+    if ( cell8.innerText.trim().length !== 0 ){
+      return false;
+    }
+    cell8.innerText = move === "player1" ? "X" : "O";
+    move = move === "player1" ? "player2" : "player1";
+  });  
+
+  cell9.addEventListener("click", event =>{
+    if ( cell9.innerText.trim().length !== 0 ){
+      return false;
+    }
+    cell9.innerText = move === "player1" ? "X" : "O";
+    move = move === "player1" ? "player2" : "player1";
+  }); 
+}
