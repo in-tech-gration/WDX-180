a6089fe3579aabf898dd1bd3f7ef9d34730fd95c
Kostas Minaidis <minaidisk@gmail.com>
2026-01-22 13:21:49 +0200

    Update resources UI: sidebar with dynamic entries

--- resources/ui/sidebar.js
+++ resources/ui/sidebar.js
@@ -1,3 +1,29 @@
+const entries = [
+  { type: "YouTube Videos", count: 56, checked: true },
+  { type: "YouTube Playlists", count: 10, checked: false },
+  { type: "Blog Posts", count: 20, checked: true },
+];
+let entriesHTML = "";
+let counter = 0;
+
+for ( const entry of entries ){
+
+  counter++;
+  const isChecked = entry.checked;
+  let checked = ""; 
+  if ( isChecked ){
+    checked = "checked";
+  }
+  entriesHTML += `
+    <div class="type-container">
+      <input type="checkbox" id="resource${counter}" class="job-style" ${checked}>
+      <label for="resource${counter}">${entry.type}</label>
+      <span class="job-number">${entry.count}</span>
+    </div>
+  `
+
+}
+
 const html = `
   <div class="alert">
     <div class="alert-title">Search by title</div>
@@ -7,40 +33,10 @@ const html = `
   </div>
   <div class="job-time">
     <div class="job-time-title">Type of Resource</div>
-    <div class="job-wrapper">
-      <div class="type-container">
-        <input type="checkbox" id="job1" class="job-style" checked>
-        <label for="job1">YouTube Videos</label>
-        <span class="job-number">56</span>
-      </div>
-      <div class="type-container">
-        <input type="checkbox" id="job2" class="job-style">
-        <label for="job2">YouTube Playlists</label>
-        <span class="job-number">43</span>
-      </div>
-      <div class="type-container">
-        <input type="checkbox" id="job3" class="job-style">
-        <label for="job3">Blog Posts</label>
-        <span class="job-number">24</span>
-      </div>
-      <div class="type-container">
-        <input type="checkbox" id="job4" class="job-style">
-        <label for="job4">Internship Jobs</label>
-        <span class="job-number">27</span>
-      </div>
-      <div class="type-container">
-        <input type="checkbox" id="job5" class="job-style">
-        <label for="job5">Contract</label>
-        <span class="job-number">76</span>
-      </div>
-      <div class="type-container">
-        <input type="checkbox" id="job6" class="job-style">
-        <label for="job6">Training Jobs</label>
-        <span class="job-number">28</span>
-      </div>
-    </div>
+    <div class="job-wrapper">${entriesHTML}</div>
   </div>
 `
+
 
 const searchTypeEl = document.querySelector(".search-type");
 
