cba53378aa805273f7e8ae6989b82416938cf224
Nicolas Wickens <nicolaswickens@gmail.com>
2026-01-29 00:46:51 +0200

    still have a small problem

--- resources/ui/sidebar.js
+++ resources/ui/sidebar.js
@@ -1,8 +1,9 @@
 // We need access to the JSON data, 'data.resources'
 
-function findType(data) {
+
+function findType(resources) {
   let types = [];
-  Object.values(data.resources).forEach((resource) => {
+  Object.values(resources).forEach((resource) => {
     if (!types.includes(resource.type)) {
       types.push(resource.type);
     }
@@ -10,12 +11,12 @@ function findType(data) {
   return types;
 }
 
-function findTypeRepeat(data, types) {
+function findTypeRepeat(resources, types) {
   let counter;
   let entries = [];
   types.forEach((type) => {
     counter = 0;
-    Object.values(data.resources).forEach((resource) => {
+    Object.values(resources).forEach((resource) => {
       if (resource.type === type) {
         counter++;
       }
@@ -23,14 +24,12 @@ function findTypeRepeat(data, types) {
     let entry = { type: type, count: counter, checked: true };
     entries.push(entry);
   });
-  console.log(entries);
   return entries;
 }
 
 function loopForEntries(entries) {
   let entriesHTML = "";
   let counter = 0;
-  console.log(entries);
   for (const entry of entries) {
     counter++;
     const isChecked = entry.checked;
@@ -49,52 +48,85 @@ function loopForEntries(entries) {
   return entriesHTML;
 }
 
-function searchButton(data){
+function insertHTML(entriesHTML) {
+  const html = `
+  <div class="alert">
+  <div class="alert-title">Search by title</div>
+  <div class="alert-subtitle">Enter a keyword to search resource titles</div>
+  <input type="text" placeholder="Enter title keyword">
+  <button class="search-buttons">Search</button>
+  </div>
+  <div class="job-time">
+    <div class="job-time-title">Type of Resource</div>
+    <div class="job-wrapper">${entriesHTML}</div>
+  </div>
+      `;
+  const searchTypeEl = document.querySelector(".search-type");
+  searchTypeEl.innerHTML = "";
+  searchTypeEl.innerHTML = html;
+}
+
+function checkResult(resources) {
+  console.log(resources);
+  document.querySelector(".job-wrapper").addEventListener("click", (event) => {
+    if (event.target.id.includes("resource")) {
+      let checkboxes = document.querySelectorAll(".job-wrapper input");
+      let checkedResult = {};
+      checkboxes.forEach((checkbox) => {
+        const labelElement = document.querySelector(
+          `.type-container label[for='${checkbox.id}']`,
+        );
+        let checkedText = labelElement.textContent;
+        if (checkbox.checked === true) {
+          Object.entries(resources).forEach(([key, value]) => {
+            if (value.type === checkedText) {
+              checkedResult[key] = value;
+            }
+          });
+        }
+      })
+      console.log(checkedResult);
+    }
+  });
+}
+
+function searchButton(data, resources) {
   let inputElement = document.querySelector(".alert input");
-  let buttonElement  = document.querySelector(".alert button");
+  let buttonElement = document.querySelector(".alert button");
   let searchResult;
   buttonElement.addEventListener("click", () => {
     searchResult = {};
-    Object.entries(data.resources).forEach(([key, value]) => {
-      if (key.includes(inputElement.value)) {
-        searchResult[key] = value;
-      }
-    });
+    if (inputElement.value) {
+      Object.entries(resources).forEach(([key, value]) => {
+        if (key.includes(inputElement.value)) {
+          searchResult[key] = value;
+        }
+      });
+    } else {
+      searchResult = data.resources;
+    }
+    // console.log(searchResult);
+    sidebarAPI(data, searchResult);
   });
-  return searchResult;
 }
 
-
-
-function renderSidebar(data) {
-  console.log(data.resources);
+function sidebarAPI(data, resources) {
+  console.log("entered");
   // this function find how many types do we have, return an array of types we have
-  let types = findType(data);
+  let types = findType(resources);
   // this function find how many times each type repeat, return an array of object of the info of each type
-  let entries = findTypeRepeat(data, types);
+  let entries = findTypeRepeat(resources, types);
   // this function iterate though the array and put it into the HTML, return the HTML
   let entriesHTML = loopForEntries(entries);
+  // this function will insert HTML
+  insertHTML(entriesHTML);
+  checkResult(resources);
+  searchButton(data, resources); // this might be useful later
+}
 
-  const html = `
-    <div class="alert">
-      <div class="alert-title">Search by title</div>
-      <div class="alert-subtitle">Enter a keyword to search resource titles</div>
-      <input type="text" placeholder="Enter title keyword">
-      <button class="search-buttons">Search</button>
-    </div>
-    <div class="job-time">
-      <div class="job-time-title">Type of Resource</div>
-      <div class="job-wrapper">${entriesHTML}</div>
-    </div>
-  `;
-
-  const searchTypeEl = document.querySelector(".search-type");
-
-
-
-  searchTypeEl.innerHTML = html;
-  let searchResult = searchButton(data);// this might be useful later
-  let checkResult = checkResult(data, searchResult);
+function renderSidebar(data) {
+  // console.log(data.resources);
+  sidebarAPI(data, data.resources);
 }
 
 export default renderSidebar;
