0f7e6fec8b169c79db69fb2934030ae9cdcdf8f4
Kostas Minaidis <minaidisk@gmail.com>
2026-05-25 01:21:35 +0300

    W20D03 Express

---
+++ curriculum/modules/javascript/misc/_w20d03/assets/day03/intro-to-express/components/menu.js
@@ -0,0 +1,8 @@
+const menu = `<nav>
+    <a href="/">Home</a>
+    <a href="/gallery">Gallery</a>
+    <a href="/contact">Contact</a>
+    <a href="/about-us">About</a>
+  </nav>`;
+
+module.exports = menu;
